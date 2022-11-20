a7b680a3c0c05a667ba25e5c8994505ada854dd4
peemo <peemouze@gmail.com>
2022-11-20 14:54:48 +0700

    Добавил фото и изменил get_user_text

--- main.py
+++ main.py
@@ -4,23 +4,22 @@ from telebot import  types
 bot = telebot.TeleBot('5700369150:AAGVPuoA7v2DJneTNUBu1R1uzwGpWk23S-w')
 
 @bot.message_handler(commands=['start'])
-
 def start(message):
     mess = f'Привет, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
 
-# @bot.message_handler(content_types=['text'])
-# def get_user_text(message):
-#     if message.text == 'Hello':
-#         bot.send_message(message.chat.id, "И тебе привет!", parse_mode='html')
-#     elif message.text == "id":
-#         bot.send_message(message.chat.id, f"Твой ID: {message.from_user.id}", parse_mode='html')
-#     elif message.text == "photo":
-#         photo = open('icon.png', 'rb')
-#         bot.send_photo(message.chat.id, photo)
-#     else:
-#         bot.send_message(message.chat.id, "Я тебя не понимаю", parse_mode='html')
+@bot.message_handler(content_types=['text'])
+def get_user_text(message):
+    if message.text == 'Привет':
+        bot.send_message(message.chat.id, "И тебе привет!", parse_mode='html')
+    elif message.text == "ID":
+        bot.send_message(message.chat.id, f"Твой ID: {message.from_user.id}", parse_mode='html')
+    elif message.text == "Фото":
+        photo = open('image.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
+    else:
+        bot.send_message(message.chat.id, "Я тебя не понимаю", parse_mode='html')
 
 @bot.message_handler(content_types=['photo'])
 def get_user_photo(message):
